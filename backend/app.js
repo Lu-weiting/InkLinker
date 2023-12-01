f7790f048ae2c6e98fade2e434c2a3e47c2649cb
Lu-weiting <runeeld23@gmail.com>
2023-12-01 17:55:51 +0800

    add SSL set

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-
+const path = require('path');
 
 app.use(cors());
 app.use(express.json());
@@ -10,9 +10,9 @@ app.get('/api/1.0/test', (req, res) => {
     res.send('Hello');
 });
 
-app.get('/.well-known/pki-validation/753A3038A7992A7112828484D232D6CA.txt', (req, res) => {
+app.get('/.well-known/pki-validation/444574AA09C845D69B87F522F342F2BB.txt', (req, res) => {
     console.log("well-know!");
-    const file= path.join(__dirname,'static','753A3038A7992A7112828484D232D6CA.txt');
+    const file= path.join(__dirname,'static','444574AA09C845D69B87F522F342F2BB.txt');
     console.log(file);
     res.sendFile(file);
 });
