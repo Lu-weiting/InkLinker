cbda3f6d2315389be03d0ee7203b2ad358206b00
Lu-weiting <runeeld23@gmail.com>
2023-12-15 11:17:53 +0800

    Fix: application bug

--- backend/Repository/mlModelRepo.js
+++ backend/Repository/mlModelRepo.js
@@ -46,7 +46,7 @@ module.exports = {
 
             const selectQuery = `
                 SELECT 
-                    pl.user_id,
+                    u.id AS user_id,
                     pl.post_id,
                     p.main_category AS category,
                     IF(pl.post_id IS NOT NULL AND pl.user_id = u.id, 1, 0) AS \`like\`
