e38b3f91ecacef02ee7a42f30377d0cc89e4b7e5
Lu-weiting <runeeld23@gmail.com>
2023-12-20 14:02:52 +0800

    Feat: add Read page

--- backend/utils/modelDefinition.js
+++ backend/utils/modelDefinition.js
@@ -9,9 +9,9 @@ function createModel(userCount, articleCount, categoryCount, embeddingSize) {
     const articleEmbedding = tf.layers.embedding({inputDim: articleCount, outputDim: embeddingSize}).apply(articleInput);
 
     const categoryInput = tf.input({shape: [categoryCount]});
-    const categoryEmbedding = tf.layers.dense({units: embeddingSize, activation: 'relu'}).apply(categoryInput);
+    const categoryDense = tf.layers.dense({units: embeddingSize, activation: 'relu'}).apply(categoryInput);
 
-    const concatenated = tf.layers.concatenate().apply([tf.layers.flatten().apply(userEmbedding), tf.layers.flatten().apply(articleEmbedding), tf.layers.flatten().apply(categoryEmbedding)]);
+    const concatenated = tf.layers.concatenate().apply([tf.layers.flatten().apply(userEmbedding), tf.layers.flatten().apply(articleEmbedding), categoryDense]);
 
     const dense1 = tf.layers.dense({units: 128, activation: 'relu'}).apply(concatenated);
     const output = tf.layers.dense({units: 1, activation: 'sigmoid'}).apply(dense1);
